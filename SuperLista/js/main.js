8d37a8cebd6ab0c39f8148bb8e9257bd3d33c770
evelyndisa <evelyndisa@icloud.com>
2024-12-26 19:12:21 -0300

    add teacher actualization

--- SuperLista/js/main.js
+++ SuperLista/js/main.js
@@ -1,77 +1,130 @@
-// -----------------------------------------------------------
-//                VARIABLES GLOBALES                
-//------------------------------------------------------------
-// listado de los productos
-
+/* -------------------------------------- */
+/*          VARIABLES GLOBALES            */
+/* -------------------------------------- */
 let listaProductos = [
-    { nombre: 'Seitan', cantidad: 1, precio: 200 },
-    { nombre: 'Jamon', cantidad: 3, precio: 200 },
-    { nombre: 'Tofu', cantidad: 5, precio: 200 },
-    { nombre: 'Medallon', cantidad: 22, precio: 200 }
+    { nombre: 'Carne', cantidad: 2, precio: 12.34 },
+    { nombre: 'Pan', cantidad: 3, precio: 34.56 },
+    { nombre: 'Fideos', cantidad: 4, precio: 78.90 },
+    { nombre: 'Leche', cantidad: 5, precio: 87.65 },
+    { nombre: 'Crema', cantidad: 6, precio: 43.21 },
 ]
 
+let crearLista = true
+let ul
 
-// -----------------------------------------------------------
-//                FUNCIONES GLOBALES                
-//------------------------------------------------------------
-function renderLista(){
-    const lista = document.getElementById('lista');
-    lista.innerHTML = ''; 
-    const ul = document.createElement('ul');
-    ul.classList.add('demo-list-icon', 'mdl-list')
+/* -------------------------------------- */
+/*          FUNCIONES GLOBALES            */
+/* -------------------------------------- */
+function borrarProd(index) {
+    //console.log('borrarProd', index)
+
+    listaProductos.splice(index, 1)
+    renderLista()
+}
+
+function cambiarValorProd(que, cual, el) {
+
+    const valor = el.value
+
+    listaProductos[cual][que] = que == 'cantidad'? parseInt(valor) : parseFloat(valor)
+}
+
+function renderLista() {
+    if(crearLista) {
+        ul = document.createElement('ul')
+        ul.classList.add('demo-list-icon', 'mdl-list')
+    }
+
+    ul.innerHTML = ''
+    listaProductos.forEach( (prod, index) => {
+        ul.innerHTML += 
+        `
+            <li class="mdl-list__item">
+                <!-- ícono del producto -->
+                <span class="mdl-list__item-primary-content w-10">
+                    <i class="material-icons mdl-list__item-icon">shopping_cart</i>
+                </span>
     
-    listaProductos.forEach((producto, index)=>{
-        ul.innerHTML += `
-        <li class="mdl-list__item">
-                        
-            <span class="mdl-list__item-primary-content w-10">
-                <i class="material-icons mdl-list__item-icon">shopping_cart</i>
-            </span>
-            
-            <span class="mdl-list__item-primary-content w-20">
-                ${producto.nombre}
-            </span>
-
-            <span class="mdl-list__item-primary-content w-30">
-                <div class="mdl-textfield mdl-js-textfield">
-                    <input value=${producto.cantidad} class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="cantidad-1">
-                    <label class="mdl-textfield__label" for="${index}">Cantidad</label>
-                </div>
-            </span>
-
-            <span class="mdl-list__item-primary-content w-30 ml-10">
-                <div class="mdl-textfield mdl-js-textfield">
-                    <input value=${producto.precio} class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id=${index}>
-                    <label class="mdl-textfield__label" for="${index}">Precio</label>
-                </div>
-            </span>
-            
-            <!-- Colored FAB button with ripple -->
-            
-            <span class="mdl-list__item-primary-content w-10 ml-10">
-                <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
-                    <i class="material-icons">remove_shopping_cart</i>
-                </button>
-            </span>
-
-        </li>
-    `;
+                <!-- nombre del producto -->
+                <span class="mdl-list__item-primary-content w-30">
+                    ${prod.nombre}
+                </span>
+    
+                <!-- cantidad de producto -->
+                <span class="mdl-list__item-primary-content w-20">
+                    <!-- Textfield with Floating Label -->
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input onblur="cambiarValorProd('cantidad',${index},this)" value="${prod.cantidad}" class="mdl-textfield__input" type="text" id="cantidad-${index}">
+                        <label class="mdl-textfield__label" for="cantidad-${index}">Cantidad</label>
+                    </div>
+                </span>
+    
+                <!-- precio de producto -->
+                <span class="mdl-list__item-primary-content w-20 ml-item">
+                    <!-- Textfield with Floating Label -->
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input onblur="cambiarValorProd('precio',${index},this)" value="${prod.precio}" class="mdl-textfield__input" type="text" id="precio-${index}">
+                        <label class="mdl-textfield__label" for="precio-${index}">Precio</label>
+                    </div>
+                </span>
+    
+                <!-- botón de borrado individual del producto en la lista -->
+                <span class="mdl-list__item-primary-content w-20 ml-item">
+                    <!-- Colored FAB button with ripple -->
+                    <button onclick="borrarProd(${index})"
+                        class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+                        <i class="material-icons">remove_shopping_cart</i>
+                    </button>
+                </span>
+            </li>
+        `
     })
-    document.getElementById('lista').append(ul)
+
+    if(crearLista) {
+        document.getElementById('lista').appendChild(ul)
+    }
+    else {
+        componentHandler.upgradeElements(ul)
+    }
+
+    crearLista = false
+}
+
+function configurarListenersMenu() {
+    /* botón agregar producto */
+    document.getElementById('btn-entrada-producto').addEventListener('click', () => {
+        const input = document.getElementById('ingreso-producto')
+        const nombre = input.value
+
+        //console.log(nombre)
+        const producto = { nombre: nombre, cantidad: 1, precio: 0 }
+        //console.log(producto)
+        listaProductos.push(producto)
+
+        renderLista()
+
+        input.value = ''
+    })
+
+    /* botón borrar todo */
+    document.getElementById('btn-borrar-productos').addEventListener('click', () => {
+        
+        if(confirm('¿Desea borrar todos los productos?')) {
+            listaProductos = []
+            renderLista()
+        }
+    })
+}
+
+function start() {
+    console.warn( document.querySelector('title').innerText )
+
+    configurarListenersMenu()
+    renderLista()
 }
 
 
-function configurarListenerMenu(){
-    //boton borrar todo
-    document.getElementById('btn-eliminar-productos').addEventListener('click', ()=>{
-        listaProductos = [];
-        renderLista();
-    });
-}
-
-
-// -----------------------------------------------------------
-//                    EJECUCIONES            
-//------------------------------------------------------------
-renderLista()
-configurarListenerMenu()
+/* -------------------------------------- */
+/*               EJECUCIÓN                */
+/* -------------------------------------- */
+start()
